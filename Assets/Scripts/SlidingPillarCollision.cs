db2f81a0e1f76749d4d78bb317a4b65aa1a1a678
rtechau3 <rtech153@gmail.com>
2019-11-17 16:25:33 -0500

    pillar position/anims

---
+++ Assets/Scripts/SlidingPillarCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingPillarCollision : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+
+    public void onTriggerEnter(Collider collider) {
+        Debug.Log("trigger");
+        if (collider.CompareTag("Player")) {
+            anim.SetBool("shouldPillarSlide", true);
+        }
+    }
+}
