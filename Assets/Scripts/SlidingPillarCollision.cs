d0b2d9af20170457c21cb9152c022e3e15afb6a9
rtechau3 <rtech153@gmail.com>
2019-11-17 19:12:01 -0500

    initial Mars config

--- Assets/Scripts/SlidingPillarCollision.cs
+++ Assets/Scripts/SlidingPillarCollision.cs
@@ -6,6 +6,10 @@ public class SlidingPillarCollision : MonoBehaviour
 {
     [SerializeField] Animator anim;
 
+    // void Update() {
+    //     Debug.Log("hello!");
+    // }
+
     public void onTriggerEnter(Collider collider) {
         Debug.Log("trigger");
         if (collider.CompareTag("Player")) {
