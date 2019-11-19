70c048a1549b4aacb45bc1637e099b22e68dc674
rtechau3 <rtech153@gmail.com>
2019-11-19 12:57:10 -0500

    sinking script and pad spacing

---
+++ Assets/Scripts/SinkingLillypads.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkingLillypads : MonoBehaviour
+{
+    bool shouldLillypadsSink;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shouldLillypadsSink = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldLillypadsSink) {
+            transform.Translate(Vector3.down * Time.deltaTime, Space.Self);
+            if (transform.position.y <= -20) {
+                // trigger reset
+                shouldLillypadsSink = false;
+            }
+        }
+    }
+}
