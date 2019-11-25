dda7e1c5dd3c6126b6ff0aaa367e29ea39a7c2e6
rtechau3 <rtech153@gmail.com>
2019-11-25 13:23:16 -0500

    red lilly anim

---
+++ Assets/Scripts/LillyColoring.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LillyColoring : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+
+    private void OnTriggerEnter(Collider c) {
+        if (c.CompareTag("Player")) {
+            anim.SetBool("shouldLillyTurnRed", true);
+        }
+    }
+}
